b98f396a009c47a7db183cf66aec2bde284fdc14
Kath86 <kathytheh@hotmail.de>
2014-07-05 17:46:56 +0200

    Update complete.R
    Write a function that reads a directory full of files and reports the number of completely observed cases in each data file. The function should return a data frame where the first column is the name of the file and the second column is the number of complete cases.

--- complete.R
+++ complete.R
@@ -1,3 +1,7 @@
+## compleWrite a function that reads a directory full of files and reports the number of completely 
+## observed cases in each data file. The function should return a data frame where the first column 
+## is the name of the file and the second column is the number of complete cases.
+
 complete <- function(directory, id = 1:332){
   directory <- "C:/Dokumente und Einstellungen/Kath/Desktop/specdata"
   files_list <- dir(path = directory,pattern = "csv", full.names = TRUE)
